b5e97814fcdf37fb421761af8767409dae1aa410
Dev2 <Dev2@gmail.com>
2025-11-08 10:43:27 +0530

    Initial commit for MavenDemo1 project

---
+++ src/main/java/com/app/MavenDemo1/Arithmentic_Operations.java
@@ -0,0 +1,11 @@
+package com.app.MavenDemo1;
+
+public class Arithmentic_Operations {
+ public static int add(int a,int b) {
+	return a+b;
+}
+ 
+ public static int sub(int a,int b) {
+	 return a-b;
+ }
+}
---
+++ src/main/java/com/app/main/Test.java
@@ -0,0 +1,11 @@
+package com.app.main;
+
+import com.app.MavenDemo1.Arithmentic_Operations;
+
+public class Test {
+public static void main(String[] args) {
+	Arithmentic_Operations aob=new Arithmentic_Operations();
+	System.out.println(aob.add(3, 2));
+	
+}
+}
